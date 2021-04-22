32cc638b4815429b7e9eaed4725c38f2818a7c05
Minhsiung <NEIL@Minhsiungw.local>
2021-04-22 20:29:32 +0800

    作業版本一

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,24 @@
+//方法一
 function capitalize(str) {
-  
+   if (str[0].charCodeAt() > 97){
+   var separateWord = str.toLowerCase().split(' ');
+      separateWord[0] = separateWord[0].charAt(0).toUpperCase() +
+      separateWord[0].substring(1)  
+      return separateWord.join(' ');
+  } else {
+  	return str
+  }
 }
+console.log(capitalize('hello'));
 
+//方法二
+function capitalize(str) {
+   if (str[0].charCodeAt() > 97){
+      str = str[0].toUpperCase() +
+      str.substring(1)  
+      return str
+  } else {
+  	return str
+  }
+}
 console.log(capitalize('hello'));
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
-function reverse(str) {
-  
+function reverse(str){
+  var r = "";
+  for(var i = str.length - 1; i >= 0; i--){
+    r += str[i];
+  }
+  console.log(r)
 }
 
 reverse('hello');
