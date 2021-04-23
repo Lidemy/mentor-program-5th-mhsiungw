945db1724e8b34f0678d13288f694ac6ff715433
mhsiungw <81913228+mhsiungw@users.noreply.github.com>
2021-04-23 16:48:09 +0800

    作業版本二

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,17 +1,3 @@
-//方法一
-function capitalize(str) {
-   if (str[0].charCodeAt() > 97){
-   var separateWord = str.toLowerCase().split(' ');
-      separateWord[0] = separateWord[0].charAt(0).toUpperCase() +
-      separateWord[0].substring(1)  
-      return separateWord.join(' ');
-  } else {
-  	return str
-  }
-}
-console.log(capitalize('hello'));
-
-//方法二
 function capitalize(str) {
    if (str[0].charCodeAt() > 97){
       str = str[0].toUpperCase() +
